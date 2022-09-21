a24624382a80de3f8699f439be490c7add248b29
Leon <leonbehrndt1@gmail.com>
2022-09-22 00:19:03 +0200

    new html elements + styling and test js function
    New html elements were created to display all elements properly. The elements were styled for a aesthetic reasons. A new js function was created that grabs all memory card elements to work with them.

--- components/JS/index.js
+++ components/JS/index.js
@@ -1,1 +1,13 @@
-
+function getMemoryCards() {
+    const memoryCards = [];
+    for(let i = 1; i <= 6; i++) {
+        let memoryCard = document.getElementById(`memory-card-${i}`);
+        memoryCards.push(memoryCard);
+    }
+    let test = memoryCards[3]
+    if(test.style.display == "none") {
+        test.style.display = "block";
+    } else {
+        test.style.display = "none";
+    }
+}
