a89000519209dc451887bffbcecd97a4470083bc
Leon <leonbehrndt1@gmail.com>
2022-09-26 15:49:18 +0200

    Completion of project
    The show() function was updated to properly display the cards in the intended memory style. A new global variable was added to store the last displayed image and a new asynch helper function was implemented to reset the current and last image if they do not match.

--- components/JS/index.js
+++ components/JS/index.js
@@ -1,4 +1,5 @@
-// Store images in their given order
+
+// Store all images in a global array
 const images = getMemoryImages();
 /**
  * The function shuffle() will swap the current image with a randomly selected image.
@@ -15,29 +16,39 @@ function shuffle() {
     // swap item at current index with item at random index
     [images[size].src, images[rand].src] = [images[rand].src, images[size].src];
   }
-  reset();
 }
 
 /**
- * The function reset() will set the image's opacity to 0, so they will not be seen.
+ * The function reset() will set the image's opacity to 0 when the reset button is being clicked.
+ * It will also reset the lastImage variable, since the reset button can be pressed at any time 
+ * during the game and therefore distrub the game that will be played after.
  */
 function reset() {
-  // Set opacity of all images to 0
-  for (const image of images) {
-    image.style.opacity = "0%";
-  }
+  // set every image's opacity to 0%.
+  images.forEach((image) => (image.style.opacity = "0%"));
+  // If 
+  lastImage = "";
 }
 
 /**
- * The function show will display the image of the clicked div-element (card) and call 
- * the memorize function.
+ * The function resetSpecific(image) will take an image name as an input and reset the specific 
+ * image object with a timeout of 0.6 seconds.
+ * 
+ * @param image the given image that will be resetted.
  */
-function show() {
-  window.onclick = (e) => {
-    image = e.target;
-    image.style.opacity = "100%";
-  };
-  // TODO: memorize function
+function resetSpecific(image) {
+  // find the image object by name
+  for (const img of images) {
+    // when found
+    if (image == img.src.substring(img.src.lastIndexOf("/") + 1)) {
+      new Promise(() => {
+        // set a timeout of .6s and change the image's opacity to 0%, so it won't be displayed.
+        setTimeout(() => {
+          img.style.opacity = "0%";
+        }, "600");
+      });
+    }
+  }
 }
 
 /**
@@ -50,4 +61,28 @@ function getMemoryImages() {
     document.getElementsByClassName("hidden-memory-image")
   );
   return imageArray;
+}
+
+/**
+ * The show() function will display an image on the screen.
+ * Depending on the last displayed image it will either continue being displayed or disappear again.
+ */
+async function show(image) {
+  // Display the image
+  image.style.opacity = "100%";
+  imageName = image.src.substring(image.src.lastIndexOf("/") + 1);
+  // If there is a last image to compare the current image to
+  if (!(lastImage == "")) {
+    // check if the last image is not the same as the current image
+    // reset both
+    if (lastImage != imageName) {
+      await resetSpecific(lastImage);
+      resetSpecific(imageName);
+    }
+    // set lastImage back to default
+    lastImage = "";
+  } else {
+    // save the current image for the next comparison
+    lastImage = imageName;
+  }
 }
